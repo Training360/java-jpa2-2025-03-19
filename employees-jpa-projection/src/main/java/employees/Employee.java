f3a12fd9d66fdb2249977e28d7b9d19329103c5e
Istvan Viczian <viczian.istvan@gmail.com>
2025-03-19 15:43:42 +0100

    Projection

---
+++ employees-jpa-projection/src/main/java/employees/Employee.java
@@ -0,0 +1,51 @@
+package employees;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.NaturalId;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.util.Objects;
+
+// SOHA NE HASZNÁLJUK: @Data
+@Getter
+@Setter
+@Entity
+@Table(name = "employees")
+@NoArgsConstructor
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    // Surrogate key
+    private Long id;
+
+    private String name;
+
+    // Natural key
+    @NaturalId
+    private String personalNumber;
+
+    public Employee(String name, String personalNumber) {
+        this.name = name;
+        this.personalNumber = personalNumber;
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Employee employee = (Employee) o;
+        return getId() != null && Objects.equals(getId(), employee.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
+}
