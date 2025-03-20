5b9077ab2cec9bdc7f2ae781d7e1f1047c621baa
Istvan Viczian <viczian.istvan@gmail.com>
2025-03-20 13:14:51 +0100

    Több one-to-many kapcsolat

--- employees-jpa-one-to-manys/src/main/java/employees/EmployeeRepository.java
+++ employees-jpa-one-to-manys/src/main/java/employees/EmployeeRepository.java
@@ -1,9 +1,6 @@
 package employees;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import java.util.List;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
--- employees-jpa-one-to-manys/src/test/java/employees/EmployeeRepositoryTest.java
+++ employees-jpa-one-to-manys/src/test/java/employees/EmployeeRepositoryTest.java
@@ -3,15 +3,11 @@ package employees;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 @SpringBootTest
-@Sql(statements = {"delete from employees", "delete from addresses", "delete from employees"})
+@Sql(statements = {"delete from phones", "delete from addresses", "delete from employees"})
 class EmployeeRepositoryTest {
 
     @Autowired
