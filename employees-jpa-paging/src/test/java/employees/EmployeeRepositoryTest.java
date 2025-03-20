c5daa291869dc934936b35f2fc8a75e61e093a3e
Istvan Viczian <viczian.istvan@gmail.com>
2025-03-20 15:43:30 +0100

    Natív JPA

--- employees-jpa-paging/src/test/java/employees/EmployeeRepositoryTest.java
+++ employees-jpa-paging/src/test/java/employees/EmployeeRepositoryTest.java
@@ -1,5 +1,7 @@
 package employees;
 
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -19,19 +21,37 @@ class EmployeeRepositoryTest {
     @Autowired
     EmployeeRepository employeeRepository;
 
-    @Test
-    void save() {
+    @Autowired
+    EntityManager entityManager;
+
+    @BeforeEach
+    void createTestData() {
         for (int i = 0; i < 50; i++) {
             var employee = new Employee("John Doe " + i, "12345" + i);
             employee.addAddress(new Address("Budapest"));
             employee.addAddress(new Address("Pécs"));
             employeeRepository.save(employee);
         }
+    }
+
+    @Test
+    void save() {
+
 
 //        var employees = employeeRepository.findAll(PageRequest.of(2, 5));
 //        var employees = employeeRepository.findAllWithAddresses(PageRequest.of(2, 5));
         var employees = employeeRepository.findAllSlicesWithAddresses(PageRequest.of(2, 5));
         System.out.println(employees.stream().map(Employee::getName).collect(Collectors.joining(", ")));
+    }
+
+    @Test
+    void nativePaging() {
+        var employees = entityManager.createQuery("select e from Employee e join fetch e.addresses order by e.name", Employee.class)
+                .setFirstResult(10)
+                .setMaxResults(5)
+                .getResultList();
+        System.out.println(employees.stream().map(Employee::getName).collect(Collectors.joining(", ")));
+
     }
 
 }
